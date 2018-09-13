946c7e1d84bb22dc87adfc6b1c5f391e891238bf
Sanjeev Kumar <kumar.sanjeev589@gmail.com>
2018-09-13 20:49:35 +0530

    Create Programming Assignment_1 Question 1.c

---
+++ Programming Assignment_1 Question 1.c
@@ -0,0 +1,39 @@
+/*
+Due on 2018-09-13, 23:59 IST
+You will be given 3 integers as input. The inputs may or may not be
+ different from each other. 
+
+You have to output 1 if all three inputs
+ are different from each other, 
+and 0 if any input is repeated more
+ than once.
+
+
+
+Input
+-----
+Three integers on three lines.
+
+
+
+Output
+------
+1 if the three inputs are different from each other
+, 0 if some input is repeated more than once
+*/
+
+
+#include<stdio.h>
+void main()
+{
+  int a,b,c;
+  scanf("%d%d%d",&a,&b,&c);
+  if(a==b||a==c||b==c)
+  {
+    printf("0");
+  }
+  else
+  {
+    printf("1");
+  }
+}
