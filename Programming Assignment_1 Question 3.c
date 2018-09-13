b055429e3363592a7e986b52fd73ed4cd3951a77
Sanjeev Kumar <kumar.sanjeev589@gmail.com>
2018-09-13 20:51:14 +0530

    Create Programming Assignment_1 Question 3.c

---
+++ Programming Assignment_1 Question 3.c
@@ -0,0 +1,30 @@
+/*
+
+Due on 2018-09-13, 23:59 IST
+A triple of numbers (a,b,c) is called a triangle triple if we can form a triangle of lengths a, b and c.
+
+In this question, you will be given three numbers. You have to output
+1 if the three numbers can form a triangle. Otherwise, you have
+to output 0.
+
+Input-----Three real numbers.
+
+Output------1 if the three numbers can form a triangle.
+                   0 otherwise
+                   
+*/
+
+#include<stdio.h>
+void main()
+{
+  float a,b,c;
+  scanf("%f%f%f",&a,&b,&c);
+  if(a+b>c&&b+c>a&&a+c>b)
+  {
+    printf("1");
+  }
+  else
+  {
+    printf("0");
+  }
+}
